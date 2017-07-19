f66e02db42772c0669e51fcff42db138a9ed84bc
Jack Wagstaffe <jackowagstaffe@gmail.com>
2017-07-20 00:06:19 +0100

    Basic app and styles.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>Spotify Tools</h1>
+        <div className="panel">
+          Use the tools below to select two playlists and compare/transform them.
+          <div className="container">
+            <div className="block">Options in here</div>
+            <div className="block">Options in here</div>
+            <div className="block">Options in here</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
