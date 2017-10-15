39772aa28bae39b32b5317259e4738a340ef5e1f
Jack Wagstaffe <jackowagstaffe@gmail.com>
2017-10-15 01:34:05 +0100

    Remove deployment script from version control.

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-export REACT_APP_CALLBACK="https://jackowagstaffe.github.io/spotify-tools/"
-npm run build
-rsync -ov /home/jack/projects/spotify_tools/build /home/jack/projects/spotify-tools-build
-cd /home/jack/projects/spotify-tools-build
-git push --set-upstream git@github.com:jackowagstaffe/spotify-tools.git master
-cd /home/jack/projects/spotofy_tools
